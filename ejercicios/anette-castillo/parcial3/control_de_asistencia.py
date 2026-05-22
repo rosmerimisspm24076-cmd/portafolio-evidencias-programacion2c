51a987972bbf3d9b3b0156dc31e3a7c7374ebeb8
Karla Ake <karla.ake_pm251932@cecyte.edu.mx>
2026-05-22 09:49:37 -0600

    agregando control de asistencia a parcial 3

---
+++ ejercicios/anette-castillo/parcial3/control_de_asistencia.py
@@ -0,0 +1,7 @@
+#crea una lista con los nombres de los alumnos 
+alumnos=("ana","luis","carlos","maria","pedro")
+
+#crea una tupla con las materias favoritas de cada alumno
+materias=("matematicas","historia","biologia","quimica","fisica")
+
+#crea un conjunto con alumnos que nunca llegara tarde
